f0d34ff079ff468ea3fa9434235f39b666558575
neison <NEISON@LAPTOP-4HHM1MMP>
2024-06-24 00:01:33 -0500

    Avance2.1BaseConexion

--- src/config/Conexion.java
+++ src/config/Conexion.java
@@ -1,5 +1,24 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 public class Conexion {
-    
+
+    Connection conector = null;
+    String usuario = "root", contraseña = "12345", cadena = "jdbc:mysql://localhost:3306/POO";
+
+    public Connection establecerConexion() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conector = DriverManager.getConnection(cadena, usuario, contraseña);
+            JOptionPane.showMessageDialog(null, "Se conecto a la base de datos.");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "error" + e.toString());
+        }
+        return conector;
+
+    }
 }
---
+++ src/config/cc.java
@@ -0,0 +1,11 @@
+package config;
+
+public class cc {
+
+    public static void main(String[] args) {
+
+        Conexion conec = new Conexion();
+        conec.establecerConexion();
+    }
+    
+}
